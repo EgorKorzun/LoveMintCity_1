3a6022ae8a398ede235a222f3e9f6b64f42a508f
EgorKorzun <egorkorzun93@gmail.com>
2021-09-26 09:58:38 +0300

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,43 @@
+function burger(n) {
+    var n;
+    n.classList.toggle('-is-active'); 
+  }
+
+  function coment(m) {
+    var m;
+    m.classList.toggle('activ_coment'); 
+  }
+
+function replace_text_1() {
+    document.getElementById("demo").innerHTML = "Большой выбор завтраков! Безглютеновые десерты, без сахара&nbsp;&mdash; это прекрасно. Были семьей из&nbsp;5&nbsp;человек и&nbsp;всем все понравилось. Обслуживание ненавязчивое и&nbsp;легкое, есть детская комната. Сочетание вкуса, десертов, атмосферы и&nbsp;красоты, а&nbsp;также приятный бонус&nbsp;&mdash; кофе."; 
+    document.getElementById("demo_number").innerHTML = "1";
+}
+function replace_text_2() {
+    document.getElementById("demo").innerHTML = "Ваш рот никогда не&nbsp;наполнится чем-то более вкусным, чем здешние десерты. Если у&nbsp;вас есть сердечко, то&nbsp;оно обязательно дрогнет и&nbsp;навеки останется тут. Саня лучший! Без него в&nbsp;моей жизни не&nbsp;случился&nbsp;бы Сахар ????"; 
+    document.getElementById("demo_number").innerHTML = "2";
+}
+function replace_text_3() {
+    document.getElementById("demo").innerHTML = "Были первый раз, по&nbsp;рекомендации подруги . Внутри красивый интерьер , персонал внимательный, особенно официант Александр. Взяла суп с&nbsp;Камчатским крабом и&nbsp;десерт с&nbsp;инжиром&nbsp;&mdash; все понравилось ! Обязательно вернёмся)"; 
+    document.getElementById("demo_number").innerHTML = "3";
+}
+function replace_text_4() {
+    document.getElementById("demo").innerHTML = "Все было очень профессионально и&nbsp;быстро! Карта интересная и&nbsp;нестандартная, поэтому падарок был невероятно итересный и&nbsp;неожиданный! Опытные специалисты, большой выбор и&nbsp;персональное обслуживание! Спасибо за&nbsp;Ваш труд)"; 
+    document.getElementById("demo_number").innerHTML = "4";
+}
+function replace_text_5() {
+    document.getElementById("demo").innerHTML = "Ооочень приятное и&nbsp;атмосферное место) Вкусная еда, интересный интерьер, прекрасный персонал. Десерты&nbsp;&mdash; безусловный маст И&nbsp;да, оно достаточно инстаграммное ????"; 
+    document.getElementById("demo_number").innerHTML = "5";
+}
+function replace_text_6() {
+    document.getElementById("demo").innerHTML = "Пришли с&nbsp;дочкой ради облаков в&nbsp;зале&nbsp;&mdash; остались благодаря шикарным десертам и&nbsp;быстрому обслуживанию. Молочные коктейли и&nbsp;торты одни из&nbsp;самых вкусных в&nbsp;городе. Официант Илья оставил приятное впечатление. Обязательно вернемся снова!"; 
+    document.getElementById("demo_number").innerHTML = "6";
+}
+function replace_text_7() {
+    document.getElementById("demo").innerHTML = "Стильное, приятное кафе в&nbsp;Название. С&nbsp;удовольствием посещаем. За&nbsp;столиком удобно отдохнуть, обсудить увиденное в&nbsp;музейных экспозициях. Хороший кофе и&nbsp;замечательные десерты. Десерты фирменные, интересные."; 
+    document.getElementById("demo_number").innerHTML = "7";
+}
+function replace_text_8() {
+    document.getElementById("demo").innerHTML = "Замечательный Том&nbsp;Ям, очень ОЧЕНЬ вкусненький)) нежная утиная грудка ???????? Очень внимательные и&nbsp;приветливые официанты. Десерты ???? Общий антураж ???????? Ненавязчивая музыка, приглушённый свет, точечная подсветка над столами... атмосфера очень располагает))"; 
+    document.getElementById("demo_number").innerHTML = "8";
+}
+
